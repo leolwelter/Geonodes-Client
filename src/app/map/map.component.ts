3352ed78e546c66eb0bbabc111eb4b8cb0c135f6
leolwelter <leolwelter@gmail.com>
2021-02-15 15:40:33 -0500

    Remove leaflet.heat. Install heatmap.js. Add MapComponent methods to fetch from the Geonode API, convert, and set heatmap.js layer data.

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -1,7 +1,15 @@
 import {Component, OnInit} from '@angular/core';
-import {circle, latLng, LatLngBounds, LeafletEvent, polygon, tileLayer} from 'leaflet';
+import {latLng, LatLngBounds, LeafletEvent, tileLayer} from 'leaflet';
 import {GeonodeService} from '../_services/geonode.service';
 import {Geonode} from '../_types/Geonode';
+
+declare var HeatmapOverlay: any; // global UMD variable from heatmap.js
+
+interface HeatmapNode {
+  lat: number;
+  lng: number;
+  count: number;
+}
 
 @Component({
   selector: 'app-map',
@@ -15,8 +23,18 @@ export class MapComponent implements OnInit {
   }
 
   mapOptions: any;
+  layers: any;
+  heatmapLayer = new HeatmapOverlay({
+    radius: .1,
+    maxOpacity: 0.5,
+    scaleRadius: true,
+    useLocalExtrema: true,
+    latField: 'lat',
+    lngField: 'lng',
+    valueField: 'count'
+  });
   layersControl: any;
-  heatData: Geonode[] = [];
+  heatData: HeatmapNode[] = [];
 
   private static handleError(err: any): void {
     console.error(err); // TODO actually handle the error
@@ -27,15 +45,13 @@ export class MapComponent implements OnInit {
       baseLayers: {
         'Open Street Map': tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {maxZoom: 18, attribution: '...'}),
         'Open Cycle Map': tileLayer('http://{s}.tile.opencyclemap.org/cycle/{z}/{x}/{y}.png', {maxZoom: 18, attribution: '...'})
-      },
-      overlays: {
-        'Big Circle': circle([46.95, -122], {radius: 5000}),
-        'Big Square': polygon([[46.8, -121.55], [46.9, -121.55], [46.9, -121.7], [46.8, -121.7]])
       }
     };
 
+
     this.mapOptions = {
       layers: [
+        this.heatmapLayer,
         tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {maxZoom: 18, attribution: '...'})
       ],
       zoom: 6,
@@ -43,10 +59,6 @@ export class MapComponent implements OnInit {
       maxZoom: 9,
       minZoom: 5
     };
-
-    // get initial heat data
-    const lltuple = this.mapOptions.center.toBounds(50000);
-    this.getHeatData(lltuple);
   }
 
   onMapMoveEnd(event: LeafletEvent): void {
@@ -55,20 +67,28 @@ export class MapComponent implements OnInit {
     this.getHeatData(bounds);
   }
 
-  private setHeatData(next: Geonode[]): void {
-    this.heatData = next;
-    // now we probably update the heat layer
-  }
-
   // fetch and subsequently set heat data from API
   private getHeatData(bounds: LatLngBounds): void {
     this.geoNodeService.getDataInBounds(bounds).subscribe(
       next => {
-        this.setHeatData(next);
+        this.setHeatDataFromGeonodes(next.results);
       },
       error => {
         MapComponent.handleError(error);
       }
     );
+  }
+
+  // set heatdata from the list provided, then refresh the layer
+  private setHeatDataFromGeonodes(geonodeList: Geonode[]): void {
+    this.heatData = geonodeList.map(gn => {
+      return {lat: gn.latitude, lng: gn.longitude, count: 1};
+    });
+    // now we update the heat layer
+    this.refreshHeatLayer();
+  }
+
+  private refreshHeatLayer(): void {
+    this.heatmapLayer.setData({data: this.heatData});
   }
 }
